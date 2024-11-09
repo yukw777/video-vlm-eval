87d4d11032165a0e51df93585ff26f68ef862019
Peter Yu <2057325+yukw777@users.noreply.github.com>
2024-11-09 14:11:38 -0800

    Set default value of rope_scaling_factor to None

--- video_vlm_eval/model/prismatic.py
+++ video_vlm_eval/model/prismatic.py
@@ -19,7 +19,7 @@ class PrismaticModel(Model[dict[str, Any]]):
         dtype: TorchDType | None = None,
         num_frame_samples: int | None = None,
         rope_scaling_type: str | None = None,
-        rope_scaling_factor: float = 2.0,
+        rope_scaling_factor: float | None = None,
         llm_backbone_ckpt_path: str | None = None,
         frames_per_seg: int | None = None,
     ) -> None:
